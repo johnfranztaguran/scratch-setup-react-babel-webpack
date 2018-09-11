21039c723299d1665d2bb2c21e3108e3d7760b5a
johnfranztaguran@gmail.com <johnfranztaguran@gmail.com>
2018-09-11 18:50:02 +0800

    user edit

--- src/components/blank.js
+++ src/components/blank.js
@@ -5,18 +5,45 @@ class Blank extends Component {
     
     constructor(props){
         super(props)
+        this.state = {
+            email: "",
+            password: "",
+            error: {
+              message: ""
+            }
+        }
         this.logout = this.logout.bind(this)
     }
 
     logout(){
         firebaseApp.auth().signOut();
     }
+    
+    deleteUser(){
+        const user = firebaseApp.auth().currentUser;
+        user.delete()
+        .then((user) => {
+            alert("successfully delete")
+            console.log("deleted", user)
+        })
+        .catch((error) => {
+            console.log("error", error)
+            this.setState({error})
+        })
+    }
 
     render() { 
         return ( 
             <div>
-            <h1>awtrawtawtaw</h1>
-            <button onClick={this.logout}>Logout</button>
+            <div>
+            <button type="button" className="login-btn" onClick={this.logout}>Logout</button>
+            <button type="button" className="login-btn" onClick={this.deleteUser}>Delete Account</button>
+            </div>
+            <h1>Hello This is your Home</h1>
+            
+            <div>
+            {this.state.error.message}
+            </div>
             </div>
          );
     }
